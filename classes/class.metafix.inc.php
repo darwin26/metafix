613b6d699ff659a39a6d5f09fb30e1bed165d465
jdlx <github@rexdev.de>
2012-05-01 05:28:28 +0200

    class instead functions..

---
+++ classes/class.metafix.inc.php
@@ -0,0 +1,121 @@
+<?php
+
+  /**
+   * undocumented class
+   *
+   * @package default
+   * @author
+   **/
+  class metafix
+  {
+    public $missing_fields;
+    public $orphaned_fields;
+    public $table_metas;
+    public $metainfo_metas;
+    private $types;
+
+    function __construct()
+    {
+      $this->types = array(
+        'art_' =>'rex_article',
+        'cat_' =>'rex_article',
+        'med_' =>'rex_file'
+        );
+      $this->table_metas     = self::get_table_metas();
+      $this->metainfo_metas  = self::get_metainfo_metas();
+      $this->missing_fields  = self::get_missing_fields();
+      $this->orphaned_fields = self::get_orphaned_fields();
+    }
+
+      /**
+       * undocumented function
+       *
+       * @return void
+       * @author
+       **/
+      function get_table_metas()
+      {
+        $metas = array();
+        $db = new rex_sql;
+
+        foreach ($this->types as $prefix => $table)
+        {
+         foreach($db->getDbArray('SHOW COLUMNS FROM `'.$table.'` LIKE \''.str_replace('_','\_',$prefix).'%\';') as $column)
+         {
+           $metas[$prefix][] = $column['Field'];
+         }
+       }
+
+       foreach ($this->types as $prefix => $table)
+       {
+        $metas[$prefix] = !isset($metas[$prefix]) ? array() : $metas[$prefix];
+        sort($metas[$prefix]);
+      }
+      ksort($metas);
+
+      return $metas;
+    }
+
+
+      /**
+       * undocumented function
+       *
+       * @return void
+       * @author
+       **/
+      function get_metainfo_metas()
+      {
+        $metas = array();
+        $db = new rex_sql;
+
+        foreach ($this->types as $prefix => $table)
+        {
+         foreach($db->getDbArray('SELECT `name` FROM `rex_62_params` WHERE `name` LIKE \''.str_replace('_','\_',$prefix).'%\';') as $column)
+         {
+           $metas[$prefix][] = $column['name'];
+         }
+       }
+
+       foreach ($this->types as $prefix => $table)
+       {
+        $metas[$prefix] = !isset($metas[$prefix]) ? array() : $metas[$prefix];
+        sort($metas[$prefix]);
+      }
+      ksort($metas);
+
+      return $metas;
+    }
+
+      /**
+       * undocumented function
+       *
+       * @return void
+       * @author
+       **/
+      function get_missing_fields()
+      {
+        $missing = array();
+        foreach ($this->types as $prefix => $table)
+        {
+          $missing[$prefix] = array_diff($this->metainfo_metas[$prefix],$this->table_metas[$prefix]);
+        }
+        return $missing;
+      }
+
+      /**
+       * undocumented function
+       *
+       * @return void
+       * @author
+       **/
+      function get_orphaned_fields()
+      {
+        $orphaned = array();
+        foreach ($this->types as $prefix => $table)
+        {
+          $orphaned[$prefix] = array_diff($this->table_metas[$prefix],$this->metainfo_metas[$prefix]);
+        }
+        return $orphaned;
+      }
+
+  } // END class metafix
